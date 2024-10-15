7fffd33cad95c18fad143f1867cef0705e6c10e5
Mahdhi, Mabrouk <m.mahdhi@boservice.de>
2024-10-15 21:32:51 +0200

    COMPONENTS: Prompt

---
+++ src/CodeComplete/CodeComplete/Components/PromptComponent.razor.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------
+// Copyright (c) B&O Service AG.
+// Made with love for DevConf 2024.
+// -----------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace CodeComplete.Components
+{
+    public partial class PromptComponent : ComponentBase
+    {
+        private string promptText;
+
+        private void SendPrompt()
+        {
+
+        }
+    }
+}
--- src/CodeComplete/CodeComplete/Program.cs
+++ src/CodeComplete/CodeComplete/Program.cs
@@ -1,3 +1,12 @@
+// -----------------------------------
+// Copyright (c) B&O Service AG.
+// Made with love for DevConf 2024.
+// -----------------------------------
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
