f9dd4f5514d261bf3629e8bc1a7a5937e6d1117d
Mahdhi, Mabrouk <m.mahdhi@boservice.de>
2024-10-15 21:46:53 +0200

    COMPONENTS: Console

---
+++ src/CodeComplete/CodeComplete/Components/ConsoleComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------
+// Copyright (c) B&O Service AG.
+// Made with love for DevConf 2024.
+// -----------------------------------
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace CodeComplete.Components
+{
+    public partial class ConsoleComponent : ComponentBase
+    {
+        private List<string> logs = new();
+
+        protected override void OnInitialized()
+        {
+            logs.Add("System initialized.");
+            logs.Add("User connected.");
+            logs.Add("Data loaded.");
+            logs.Add("Error: Unable to connect to the server.");
+        }
+    }
+}
